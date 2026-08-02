ab191200b7e74d3ca2d4aaed5f34e4622f29f59a
Neeraj kumar R <neeraj.roja@icloud.com>
2026-08-02 19:13:40 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,12 @@
+
+variable = 3
+print(type(variable))
+variable = 3.3
+print(type(variable))
+variable = False
+print(type(variable))
+variable = ""
+print(type(variable))
+variable = []
+print(type(variable))
+
