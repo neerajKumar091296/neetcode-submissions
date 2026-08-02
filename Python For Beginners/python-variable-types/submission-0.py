662a6453c796378509539eb6e39c09ea54d27ab4
Neeraj kumar R <neeraj.roja@icloud.com>
2026-08-02 19:12:12 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = 3
+float_type = 3.3
+bool_type = True
+string_type = ""
+list_type = []
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
