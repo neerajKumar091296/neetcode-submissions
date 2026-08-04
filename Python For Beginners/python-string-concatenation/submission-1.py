f591eb8268b8ebbb1dfbb0d56e51d72a6f775968
Neeraj kumar R <neeraj.roja@icloud.com>
2026-08-04 22:25:11 +0530

    Add: python-string-concatenation - submission-1

---
+++ Python For Beginners/python-string-concatenation/submission-1.py
@@ -0,0 +1,15 @@
+def concatenate(s1: str, s2: str) -> str:
+
+    word = s1+s2
+    if len(word) <= 10:
+        return word
+    
+    return "Too long!"
+
+
+
+
+# do not modify below this line
+print(concatenate("He", "llo"))
+print(concatenate("Hello ", "world!"))
+print(concatenate("Length", "of10"))
